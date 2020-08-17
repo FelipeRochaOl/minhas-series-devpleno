dc387436cfec27ea091e0554724d1d66269af47d
felipe.oliveira <felipe.oliveira@westwing.com.br>
2020-08-17 03:05:14 -0300

    Adicionado Debug

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = process.env.PORT || 9000;
-const mongo = process.env.MONGODB || "mongodb://mongo/minhas-series";
 
-const mongoose = require("mongoose");
+const mongo = {
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+};
+
+const mongoConnect =
+  process.env.MONGODB ||
+  `mongodb://${mongo.host}:${mongo.port}/${mongo.database}`;
 
 const series = require("./routes/series");
 
-app.get("/", (req, res) => res.send("ExpressJS"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.get("/", (req, res) => res.send("ExpressJS"));
 app.use("/series", series);
 
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).json({ message: "Houve um erro com a aplicação!" });
+});
+
 mongoose
-  .connect(mongo, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongoConnect, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(port, () => console.log("Express listening in port " + port));
   })
